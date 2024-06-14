6fd83ec720c3381957cb224051e3f56566fd4b19
Nicole <richter.nicole.d@gmail.com>
2024-06-14 14:52:03 +0200

    added lucid-react into dev package.json added import react

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
+import React from 'react';
 import RecipeDetail from './components/recipe/RecipeDetail';
 import { ThemeProvider } from './context/ThemeContext';
 
